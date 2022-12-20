3c1907c10fc8a8225ed76c294406f58abb12ddd3
daiki <daiki.20000727@docomo.ne.jp>
2022-12-20 20:38:35 +0900

    add build

---
+++ build/precache-manifest.c93c296cd84968753be15b8e702b3db4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2cf40f2a2dbf833ce4e",
+    "url": "/static/css/main.c566d9c5.chunk.css"
+  },
+  {
+    "revision": "c2cf40f2a2dbf833ce4e",
+    "url": "/static/js/main.c2cf40f2.chunk.js"
+  },
+  {
+    "revision": "df89dab7376bae71f037",
+    "url": "/static/js/1.df89dab7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3ad87583ff6a7a5864113f6d28d60a64",
+    "url": "/index.html"
+  }
+];
